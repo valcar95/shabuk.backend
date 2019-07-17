01983a8135e4ab9dd824b230b4555a7e4d89e61d
valcar95 <va.cv95@gmail.com>
2019-07-17 13:00:09 -0500

    creación del proyecto

---
+++ shabuk/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShabukConfig(AppConfig):
+    name = 'shabuk'
---
+++ shabukBackend/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for shabukBackend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shabukBackend.settings')
+
+application = get_wsgi_application()
